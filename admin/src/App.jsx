5813179921ad5a04e3d6b7ef4ecc595ca1873ab5
Abhishekkj35 <abhishekkj6810@gmail.com>
2025-01-23 23:55:07 +0530

    final

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
   
 
-export const backendUrl=import.meta.env.VITE_BACKEND_URL
+export const backendUrl = 'https://wardrobe-backend-v3dw.onrender.com';
 export const currency = "$"
 
 const App = () => {
